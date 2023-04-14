2865f4384b4a7da11667d66226d53778186c03ff
ganajayant <ganajayant28@gmail.com>
2023-04-15 01:31:59 +0530

    added docker

--- client/src/AxiosConfig.js
+++ client/src/AxiosConfig.js
@@ -4,7 +4,7 @@ const getBaseUrl = () => {
     let url;
     switch (process.env.REACT_APP_ENV) {
         case 'production':
-            url = 'https://celestial-tvt7.onrender.com/';
+            url = 'https://backend-vtpg.onrender.com/';
             break;
         case 'development':
         default:
